daa8d2f428cb16e4e008dc47a479481e921c5ac5
Jacob <jacob@yakovliam.com>
2020-05-11 19:06:37 -0400

    Added client main class & src, fixed git issue #3

---
+++ client/src/main/java/com/yakovliam/rtoolclient/RToolClient.java
@@ -0,0 +1,13 @@
+package com.yakovliam.rtoolclient;
+
+public class RToolClient {
+
+    /**
+     * Entry point
+     *
+     * @param args CommandLine arguments created with {-arg-here}
+     */
+    public static void main(String[] args) {
+
+    }
+}
